3422c1c09dc8e4a63c55a6a97452df2cfbf75505
Florian Friedrich <friedrich@procster.de>
2024-12-02 10:02:31 +0100

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,56 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+
+        for (line in input) {
+            val splitLine = line.split(" ").filter { it.isNotBlank() }
+            left.add(splitLine[0].toInt())
+            right.add(splitLine[1].toInt())
+        }
+
+        left.sort()
+        right.sort()
+
+        var result = 0
+        for (i in 0..<left.size) {
+            result += abs(left[i] - right[i])
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+
+        for (line in input) {
+            val splitLine = line.split(" ").filter { it.isNotBlank() }
+            left.add(splitLine[0].toInt())
+            right.add(splitLine[1].toInt())
+        }
+
+        left.sort()
+        right.sort()
+
+        var result = 0
+        for (leftValue in left) {
+            val simScore = leftValue * right.count { it == leftValue }
+            result += simScore
+        }
+
+        return result
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
